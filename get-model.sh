6f1192579d52655937b6b88b146715df7366fc56
Shane Lenagh <shanelenagh@gmail.com>
2025-10-07 12:55:38 -0500

    Working LLM interaction main method

--- get-model.sh
+++ get-model.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -kL -O "https://hf.co/ibm-granite/granite-3.3-2b-instruct-GGUF/resolve/main/granite-3.3-2b-instruct-Q5_1.gguf?download=true"
+curl -kL -O "https://hf.co/ibm-granite/granite-3.3-2b-instruct-GGUF/resolve/main/granite-3.3-2b-instruct-Q5_K_M.gguf?download=true"
--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,38 @@
 #![allow(unnecessary_transmutes)]
 #![allow(unsafe_op_in_unsafe_fn)]
 use std::{ffi::CString, os::raw::c_char};
+use argh::FromArgs;
 
-include!(concat!(env!("OUT_DIR"), "/bindings.rs")); // llama.cpp bindings
+include!(concat!(env!("OUT_DIR"), "/bindings.rs")); // bindgen llama.cpp bindings
+
+#[derive(FromArgs)]
+/// Llama.cpp wrapper
+struct LlamaArgs {
+    /// model file (GGUF format)
+    #[argh(option, short='m')]
+    model: String,
+    /// prompt or question to ask model (leaving out causes it to read from stdin)
+    #[argh(option, short='p')]
+    prompt: Option<String>
+}
 
 pub unsafe fn convert_str(input: &str) -> *mut c_char {
     return CString::new(input).unwrap().into_raw();
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: LlamaArgs = argh::from_env();
     unsafe {
-        let model_params: llama_model_params = llama_model_default_params();
-        start_llama(convert_str("granite-3.3-2b-instruct-Q5_1.gguf"), model_params);
+        let mut model_params: llama_model_params = llama_model_default_params();
+        model_params.n_gpu_layers = 99;
+        start_llama(convert_str(&args.model), model_params);
+        if let Some(prompt) = args.prompt {
+            let model_context: llama_context_params = llama_context_default_params();
+            let sampler_params: llama_sampler_chain_params = llama_sampler_chain_default_params();
+            println!(">>>>>>>>>> about to run gen for model [{}] and prompt [{}]", args.model, prompt);
+            let result = run_generation(convert_str(&prompt), 1000, model_context, sampler_params);
+            println!("Got result: {}", CString::from_raw(result).into_string()?);
+        }
     }
+    Ok(())
 }
