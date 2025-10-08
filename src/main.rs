430b71433a708c28dbe0b161ed2977274ccf4017
Shane Lenagh <shanelenagh@gmail.com>
2025-10-08 11:54:38 -0500

    A little cleanup for llama bindgen code warnings, limiting to new mod created for the it alone

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-#![allow(unnecessary_transmutes)]
-#![allow(unsafe_op_in_unsafe_fn)]
 use std::{io, io::BufRead, ffi::CString, os::raw::c_char};
 use argh::FromArgs;
+#[allow(non_upper_case_globals)]
+#[allow(non_camel_case_types)]
+#[allow(non_snake_case)]
+#[allow(unused_imports)]
+#[allow(unnecessary_transmutes)]
+#[allow(unsafe_op_in_unsafe_fn)]
+#[allow(dead_code)]
+mod llama {
+    include!(concat!(env!("OUT_DIR"), "/bindings.rs")); // bindgen llama.cpp bindings
+}
+use llama::{
+    llama_model_default_params, llama_context_default_params, llama_sampler_chain_default_params, 
+    start_llama, stop_llama, run_generation
+};
 
-include!(concat!(env!("OUT_DIR"), "/bindings.rs")); // bindgen llama.cpp bindings
 
 #[derive(FromArgs)]
 /// Llama.cpp wrapper
@@ -29,11 +37,11 @@ pub unsafe fn convert_str(input: &str) -> *mut c_char {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: LlamaArgs = argh::from_env();
     unsafe {
-        let mut model_params: llama_model_params = llama_model_default_params();
+        let mut model_params = llama_model_default_params();
         model_params.n_gpu_layers = 99;
         start_llama(convert_str(&args.model), model_params);
-        let model_context: llama_context_params = llama_context_default_params();
-        let sampler_params: llama_sampler_chain_params = llama_sampler_chain_default_params();        
+        let model_context = llama_context_default_params();
+        let sampler_params = llama_sampler_chain_default_params();        
         if let Some(prompt) = args.prompt { // Single prompt given
             println!(">>>>>>>>>> about to run gen for model [{}] and prompt [{}]", args.model, prompt);
             let result = run_generation(convert_str(&prompt), args.tokens, model_context, sampler_params);
